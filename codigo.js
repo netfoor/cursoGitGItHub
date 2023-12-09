6fad02b35341c489dbe7979bf4d9fd5a7d712863
Fortino Romero <2311080254@alumno.utpuebla.edu.mx>
2023-12-09 15:48:55 -0600

    Se mejoró el archivo .js

--- codigo.js
+++ codigo.js
@@ -1,7 +1,7 @@
 alert("Bienvenido")
 let nombre = prompt("Ingresa tu nombre")
 
-let contraseña = prompt("Ingresa la contraseña. Es 123")
+let contraseña = prompt("Hola" + nombre + "  \nIngresa la contraseña. \nEs 123")
 
 if (contraseña == "123"){
     alert("Sabes seguir indicaciones")
