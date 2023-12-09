5b0475c82a4f61de2df9a497cfeeb8ffc39a2d54
Fortino Romero <2311080254@alumno.utpuebla.edu.mx>
2023-12-09 15:39:27 -0600

    Se agregó un archivo llamado codigo.js y se editó un código básico

---
+++ codigo.js
@@ -0,0 +1,10 @@
+alert("Bienvenido")
+alert("Ingresa tu nombre")
+let nombre = prompt()
+alert("Ingresa la contraseña. Es 123")
+let contraseña = prompt()
+if (contraseña == "123"){
+    alert("Sabes seguir indicaciones")
+} else {
+    prompt("Que necio eres")
+}
